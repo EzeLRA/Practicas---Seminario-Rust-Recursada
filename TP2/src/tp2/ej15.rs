0573011713090955a471aaa2a0645e6b9795ee62
ELRA <ezeleocraft@gmail.com>
2026-05-14 15:27:20 -0300

    tp2

--- TP2/src/tp2/ej15.rs
+++ TP2/src/tp2/ej15.rs
@@ -10,3 +10,26 @@ pub fn serie_geométrica<const TAMAÑO:usize>() -> [u32;TAMAÑO]{
 
     return arr;
 }
+
+#[cfg(test)]
+mod testing_ejercicio15{
+    use crate::tp2::ej15;
+    
+    #[test]
+    fn caso_especial() {
+        assert_eq!(ej15::serie_geométrica::<0>(),[0;0]);
+        assert_eq!(ej15::serie_geométrica(),[0;0]);
+    }
+
+    #[test]
+    fn arrays_geometricos(){
+        let arr1 = ej15::serie_geométrica::<1>();
+        assert_eq!(arr1.len(),1);
+        assert_eq!(arr1[0],1);
+
+        let arr1 = ej15::serie_geométrica::<5>();
+        assert_eq!(arr1.len(),5);
+        assert_eq!(arr1[arr1.len()-1],16);
+
+    }
+}
---
+++ TP2/target/debug/deps/TP2-69a215a4d48d23eb.d
@@ -0,0 +1,21 @@
+/home/e_248m/Documents/Programas/Programas Rust/Practicas---Seminario-Rust-Recursada/TP2/target/debug/deps/TP2-69a215a4d48d23eb.d: src/main.rs src/tp2/mod.rs src/tp2/ej1.rs src/tp2/ej2.rs src/tp2/ej3.rs src/tp2/ej4.rs src/tp2/ej5.rs src/tp2/ej6.rs src/tp2/ej7.rs src/tp2/ej8.rs src/tp2/ej9.rs src/tp2/ej10.rs src/tp2/ej11.rs src/tp2/ej12.rs src/tp2/ej13.rs src/tp2/ej14.rs src/tp2/ej15.rs
+
+/home/e_248m/Documents/Programas/Programas Rust/Practicas---Seminario-Rust-Recursada/TP2/target/debug/deps/TP2-69a215a4d48d23eb: src/main.rs src/tp2/mod.rs src/tp2/ej1.rs src/tp2/ej2.rs src/tp2/ej3.rs src/tp2/ej4.rs src/tp2/ej5.rs src/tp2/ej6.rs src/tp2/ej7.rs src/tp2/ej8.rs src/tp2/ej9.rs src/tp2/ej10.rs src/tp2/ej11.rs src/tp2/ej12.rs src/tp2/ej13.rs src/tp2/ej14.rs src/tp2/ej15.rs
+
+src/main.rs:
+src/tp2/mod.rs:
+src/tp2/ej1.rs:
+src/tp2/ej2.rs:
+src/tp2/ej3.rs:
+src/tp2/ej4.rs:
+src/tp2/ej5.rs:
+src/tp2/ej6.rs:
+src/tp2/ej7.rs:
+src/tp2/ej8.rs:
+src/tp2/ej9.rs:
+src/tp2/ej10.rs:
+src/tp2/ej11.rs:
+src/tp2/ej12.rs:
+src/tp2/ej13.rs:
+src/tp2/ej14.rs:
+src/tp2/ej15.rs:
